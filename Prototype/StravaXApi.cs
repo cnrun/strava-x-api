24b79f54bfda490dc78d16af2d547b5838e0287a
cfdd73 <eric@louvard.de>
2019-10-17 22:45:22 +0200

    - Retrieve activity type and id - Find activities for the whole month - Also works out of the debugger ;)

--- Prototype/StravaXApi.cs
+++ Prototype/StravaXApi.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Support.UI;
 using System.Security;
 
 namespace Prototype
@@ -42,23 +45,41 @@ namespace Prototype
             BrowserDriver.FindElement(By.Name("email")).SendKeys(Username);
             BrowserDriver.FindElement(By.Name("password")).SendKeys(new System.Net.NetworkCredential("", Password).Password);
             BrowserDriver.FindElement(By.Id("login-button")).Click();
-
+            // Wait until Login is done.
+            new WebDriverWait(BrowserDriver, TimeSpan.FromSeconds(30)).Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.XPath("//span[@class='app-icon-wrapper']"))));
         }
         public String[] getActivities(String AthleteId, String Year, String Month)
         {
-            String url = $"https://www.strava.com/athletes/{AthleteId}#interval_type?interval_type=month&interval={Year}{Month}&year_offset=0";
+            String url = $"https://www.strava.com/athletes/{AthleteId}#interval_type?chart_type=miles&interval_type=month&interval={Year}{Month}&year_offset=0";
+
             BrowserDriver.Navigate().GoToUrl(url);
-            var Elts=BrowserDriver.FindElements(By.XPath("//div[@class='activity entity-details feed-entry']"));
-            Console.WriteLine($"Elts count={Elts.Count}");
+            // Should wait for element.
+            Thread.Sleep(2000);
+
+            // Find all activity icons
+            var Elts=BrowserDriver.FindElements(By.XPath("//div[@class='entry-type-icon']"));
+            Console.WriteLine($"Elts count={Elts.Count} for {Year}/{Month}");
             foreach (IWebElement Elt in Elts)
             {
-                System.Console.WriteLine($"ActivityId={Elt.GetAttribute("id")}");
+                // locate the div for activity number
+                var ActivityNumberElt = Elt.FindElement(By.XPath("./../../.."));
+                var ActivityId=ActivityNumberElt.GetAttribute("id");
+                // for activitty with picture we have to search on step higher
+                if (String.IsNullOrEmpty(ActivityId)) {
+                    ActivityNumberElt = ActivityNumberElt.FindElement(By.XPath("./.."));
+                    ActivityId=ActivityNumberElt.GetAttribute("id");
+                }
+                // Retrieve the activity class, with that it's poosible to know the activity type
+                var ActivityTypeElt = Elt.FindElement(By.XPath("./span/span"));
+                System.Console.WriteLine($"Id={ActivityId} Class={ActivityTypeElt.GetAttribute("class")}");
             }
+            // Dummy return, everything in stdout
             return new String[0];
         }
         public void Dispose()
         {
-           BrowserDriver.Quit();
+            // Close Browser.
+            BrowserDriver.Quit();
         }
     }
 }
