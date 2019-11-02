c5ac3d4916db67299c66385b58c50bcbf15b1c2b
cfdd73 <eric@louvard.de>
2019-11-02 21:52:37 +0100

    add first definition

---
+++ Prototype/Query/ActivityRangeQuery.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Prototype.Model
+{    
+    public class ActivityRangeQuery
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string QueryId { get; set; }
+        public string AthleteId { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public ActivityRangeQuery()
+        {
+
+        }
+        override public string ToString()
+        {
+            return $"query activities for:{AthleteId} in[{DateFrom}-{DateTo}]";
+        }
+        public string Serialize(ActivityShort value)
+        {
+            return JsonSerializer.Serialize<ActivityShort>(value);
+        }
+        public string SerializePrettyPrint(ActivityShort value)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            return JsonSerializer.Serialize<ActivityShort>(value, options);
+        }
+    }
+}
