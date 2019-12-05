0ed4eae25552970b045e7844954613343ecf1680
cfdd73 <eric@louvard.de>
2019-12-05 22:20:28 +0100

    more sql queries samples

--- Prototype/misc/sql/stats.sql
+++ Prototype/misc/sql/stats.sql
@@ -10,3 +10,7 @@ Select
     (Select count(*) from dbo.ActivityShortDB ) AS "Activities",
     -- ativities count
     (Select count(*) from dbo.ActivityShortDB Where ActivityType='6') AS "Ski"
+
+-- select * from dbo.AthleteShortDB where AthleteName like 'wolf%maier%'
+-- select * from dbo.ActivityShortDB WHERE AthleteId=7512838 ORDER BY ActivityDate
+-- select count(*) from dbo.ActivityShortDB WHERE AthleteId=7512838
