3a57fb8c8f09b727fce9be71f2fa511435105646
cfdd73 <eric@louvard.de>
2019-12-05 22:17:43 +0100

    sql query for overview

--- Prototype/misc/sql/stats.sql
+++ Prototype/misc/sql/stats.sql
@@ -1,6 +1,12 @@
--- queries with "CREATED" status
-Select count(*) from dbo.ActivityQueriesDB WHERE [Status]=0
--- athlete count
-Select count(*) from dbo.AthleteShortDB
--- athlete count in activities
-Select count(Distinct AthleteId) from dbo.ActivityShortDB 
+-- [Shift][Cmd]'E' to execute from VS-Code / SQL Plugin
+Select 
+    -- queries with "CREATED" status
+    (Select count(*) from dbo.ActivityQueriesDB WHERE [Status]=0) AS "Remaining",
+    -- athlete count
+    (Select count(*) from dbo.AthleteShortDB) AS "Athletes",
+    -- athlete count in activities
+    (Select count(Distinct AthleteId) from dbo.ActivityShortDB) AS "Athletes in activities",
+    -- ativities count
+    (Select count(*) from dbo.ActivityShortDB ) AS "Activities",
+    -- ativities count
+    (Select count(*) from dbo.ActivityShortDB Where ActivityType='6') AS "Ski"
