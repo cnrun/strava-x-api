284afaedbe578dc61a3950b0de1b758f1d0944c7
cfdd73 <eric@louvard.de>
2019-12-09 16:51:31 +0100

    optimize query generation for updates

--- Prototype/Tools/QueriesGenerator.cs
+++ Prototype/Tools/QueriesGenerator.cs
@@ -70,7 +70,6 @@ namespace Prototype.Tools
             // retrieve first and last date
             DateTime minDT = queriesForPatient.First().DateFrom;
             DateTime maxDT = queriesForPatient.Last().DateFrom;
-            int UpdateNeed = 0;
             // If we already have an entry, we assume that the entry contains the first activity date, as it is expensive to retrieve its value with Selenium.
             if (queriesForPatient.Count==0)
             {
@@ -85,87 +84,64 @@ namespace Prototype.Tools
                 FromYear=minDT.Year;
                 FromMonth=minDT.Month;
             }
-
-            // an update is needed if the wanted range is not included in saved one.
-            if (new DateTime(FromYear,FromMonth,1) < minDT)
-            {
-                UpdateNeed |= 1;
-            }
-            if (new DateTime(ToYear,ToMonth,1) > maxDT)
-            {
-                UpdateNeed |= 2;
-            }
-
-            // Compare the theoric count between both date with the entered one.
-            int TotalWantedQueries;
+            Console.WriteLine($"queries enterred:{queriesForPatient.Count}/total:{db.ActivityQueriesDB.Count()}");
             if (FromYear==ToYear)
             {
-                TotalWantedQueries=Math.Abs(FromMonth-ToMonth)+1;
+                for(int month=FromMonth;month<=ToMonth;month++)
+                {
+                    AddQuery(db, AthleteId, new DateTime(FromYear,month,1), new DateTime(FromYear,month,1).AddMonths(1).AddDays(-1), queriesForPatient);
+                }
             }
             else
             {
-                TotalWantedQueries=12-FromMonth+1;
-                int NumberFullYears=ToYear-FromYear-1;
-                if (NumberFullYears>0)
-                    TotalWantedQueries+=12*NumberFullYears;
-                TotalWantedQueries=ToMonth;                
-            }
-            if (TotalWantedQueries>queriesForPatient.Count)
-            {
-                UpdateNeed |= 3;
-            }
-            
-            if (UpdateNeed==0)
-            {
-                Console.WriteLine($"SKIP ({UpdateNeed}) queries wanted:{TotalWantedQueries}/enterred:{queriesForPatient.Count}/total:{db.ActivityQueriesDB.Count()}");
-            }
-            else
-            {
-                Console.WriteLine($"queries wanted:{TotalWantedQueries}/enterred:{queriesForPatient.Count}/total:{db.ActivityQueriesDB.Count()}");
-                if (FromYear==ToYear)
+                // first year
+                for(int month=FromMonth;month<=12;month++)
                 {
-                    for(int month=FromMonth;month<=ToMonth;month++)
+                    AddQuery(db, AthleteId, new DateTime(FromYear,month,1), new DateTime(FromYear,month,1).AddMonths(1).AddDays(-1), queriesForPatient);
+                }
+                // all years after
+                for(int year=FromYear+1;year<=ToYear-1;year++)
+                {
+                    for(int month=01;month<=12;month++)
                     {
-                        AddQuery(db, AthleteId, new DateTime(FromYear,month,1), new DateTime(FromYear,month,1).AddMonths(1).AddDays(-1));
+                        AddQuery(db, AthleteId, new DateTime(year,month,1), new DateTime(year,month,1).AddMonths(1).AddDays(-1), queriesForPatient);
                     }
                 }
-                else
+                // last year
+                for(int month=01;month<=ToMonth;month++)
                 {
-                    // first year
-                    for(int month=FromMonth;month<=12;month++)
-                    {
-                        AddQuery(db, AthleteId, new DateTime(FromYear,month,1), new DateTime(FromYear,month,1).AddMonths(1).AddDays(-1));
-                    }
-                    // all years after
-                    for(int year=FromYear+1;year<=ToYear-1;year++)
-                    {
-                        for(int month=01;month<=12;month++)
-                        {
-                            AddQuery(db, AthleteId, new DateTime(year,month,1), new DateTime(year,month,1).AddMonths(1).AddDays(-1));
-                        }
-                    }
-                    // last year
-                    for(int month=01;month<=ToMonth;month++)
-                    {
-                        // from first day of month to last day of the month
-                        AddQuery(db, AthleteId, new DateTime(ToYear,month,1), new DateTime(ToYear,month,1).AddMonths(1).AddDays(-1));
-                    }
+                    // from first day of month to last day of the month
+                    AddQuery(db, AthleteId, new DateTime(ToYear,month,1), new DateTime(ToYear,month,1).AddMonths(1).AddDays(-1), queriesForPatient);
                 }
             }
+
             queriesForPatient = db.ActivityQueriesDB.Where(a => a.AthleteId==AthleteId).OrderBy(a => a.DateFrom).ToList();
-            Console.WriteLine($"✅ wanted:{TotalWantedQueries}/enterred:{queriesForPatient.Count}/total:{db.ActivityQueriesDB.Count()}");
+            Console.WriteLine($"✅ enterred:{queriesForPatient.Count}/total:{db.ActivityQueriesDB.Count()}");
         }
-        static private void AddQuery(StravaXApiContext db, String AthleteId, DateTime DateFrom, DateTime DateTo)
+        static private void AddQuery(StravaXApiContext db, String AthleteId, DateTime DateFrom, DateTime DateTo, List<ActivityRangeQuery> queriesForPatient)
         {
-            if (db.ActivityQueriesDB.Find(AthleteId,DateFrom,DateTo)==null)
+            if (!isDateInList(DateFrom,queriesForPatient))
             {
-                ActivityRangeQuery query = new ActivityRangeQuery();
-                query.AthleteId=AthleteId;
-                query.DateFrom=DateFrom;
-                query.DateTo=DateTo;
-                db.ActivityQueriesDB.Add(query);
-                Console.WriteLine($"add query for {query}");
-            }            
+                if (db.ActivityQueriesDB.Find(AthleteId,DateFrom,DateTo)==null)
+                {
+                    ActivityRangeQuery query = new ActivityRangeQuery();
+                    query.AthleteId=AthleteId;
+                    query.DateFrom=DateFrom;
+                    query.DateTo=DateTo;
+                    db.ActivityQueriesDB.Add(query);
+                    Console.WriteLine($"add query for {query}");
+                }            
+            }
+        }
+        static private bool isDateInList(DateTime DateFrom, List<ActivityRangeQuery> queriesForPatient)
+        {
+            bool ret = false;
+            foreach(ActivityRangeQuery aqr in queriesForPatient)
+            {
+                ret = aqr.DateFrom==DateFrom;
+                if (ret) break;
+            }
+            return ret;
         }
     }
 }
