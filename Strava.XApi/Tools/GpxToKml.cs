aee644935863094810d8790f050a0e0cbf18c7d3
cfdd73 <eric@louvard.de>
2020-02-23 18:09:35 +0100

    - add split argument for avoid hudge kml files. Such big files can't be loaded from google earth. - Do not add kml node when activities has not been downloaded.

--- Strava.XApi/Tools/GpxToKml.cs
+++ Strava.XApi/Tools/GpxToKml.cs
@@ -23,11 +23,14 @@ namespace Strava.XApi.Tools
         private static XNamespace gx="http://www.google.com/kml/ext/2.2";
         private static XNamespace kml = "http://www.opengis.net/kml/2.2";
         private static XNamespace atom = "http://www.w3.org/2005/Atom";
-        /**
-         * Convert GPX files to KML.
-         * GPX File should have bee retrieved with get-gpx.
-         * All GPX are merged in one KML file with subfolder for athlete and activity types.
-         */
+
+        /// <summary>
+        /// Convert GPX files to KML.
+        /// GPX File should have bee retrieved with get-gpx.
+        /// All GPX are merged in one KML file with subfolder for athlete and activity types.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static int Convert(string[] args)
         {
             var loggerFactory = LoggerFactory.Create(builder =>
@@ -43,9 +46,10 @@ namespace Strava.XApi.Tools
             var logger = loggerFactory.CreateLogger<StravaXApi>();
             logger.LogDebug("Log GpxToKml");
             string AthleteId = null;
-            // default for now
             string ActivityTypeStr = null;
+            string filenameExtention=".kml";
             string exportKmlPath = "StravaXApi.kml";
+            string splitBlockSizeStr = null;
             string exportTypeStr = ExportType.HeatMap.ToString();
             string beginDateStr = null;
             var p = new OptionSet () {
@@ -54,10 +58,15 @@ namespace Strava.XApi.Tools
                 { "e|export_type=",   v => { exportTypeStr=v; } },
                 { "bd|begin_date=",   v => { beginDateStr=v; } },
                 { "d|destination=",   v => { exportKmlPath=v; } },
+                { "s|split=",   v => { splitBlockSizeStr=v; } },
             };
             p.Parse(args);
             int ret = -1;
 
+            if (!exportKmlPath.EndsWith(filenameExtention))
+            {
+                exportKmlPath=$"{exportKmlPath}{filenameExtention}";
+            }
             ExportType exportType=(ExportType)Enum.Parse(typeof(ExportType), exportTypeStr);
 
             try
@@ -65,6 +74,8 @@ namespace Strava.XApi.Tools
                 List<ActivityShort> activities;
                 using (StravaXApiContext db = new StravaXApiContext())
                 {
+                    // Just do one query for all queries, order will be user to separate athletes and activity type.                    
+                    // [note] code is not really readable, afterthere it would have be a better idea to use several queries : athletes/activity types/activities in date range.
                     IQueryable<ActivityShort> dbs = db.ActivityShortDB.OrderBy(a=>a.AthleteId).ThenBy(a=>a.ActivityType).ThenByDescending(b=>b.ActivityDate);
                     if (ActivityTypeStr!=null)
                     {
@@ -78,12 +89,14 @@ namespace Strava.XApi.Tools
                     if (beginDateStr!=null)
                     {
                         DateTime dateTime=DateTime.Parse(beginDateStr);
-                        Console.WriteLine($"filter for date {dateTime}");
                         dbs=dbs.Where(a => a.ActivityDate>=dateTime);
                     }
+                    
                     // Ignore Activities without image map. They have probably been enterred without gps-track.
                     dbs=dbs.Where(a => a.ActivityImageMapUrl!=null);
+                    // query all activities at once.
                     activities = dbs.ToList();
+
 
                     logger.LogInformation($"BEGIN GPX➡️KML {(AthleteId==null?"all athletes":AthleteId)}/{(ActivityTypeStr==null?"all types":ActivityTypeStr)}/{(beginDateStr==null?"all dates":DateTime.Parse(beginDateStr).ToString())} :{activities.Count()}");
                     string lastAthleteId=null;
@@ -95,10 +108,32 @@ namespace Strava.XApi.Tools
                     XElement currentAthleteFolder=null;
                     XElement currentActivityTypeFolder=null;
                     int Count=0;
+                    int AthleteCount=0;
+                    int splitBlockSize=int.MaxValue;
+                    if (splitBlockSizeStr!=null)
+                    {
+                        splitBlockSize=int.Parse(splitBlockSizeStr);
+                    }
                     // go throw all activities, sorted by athlete / activity type / activity date
-                    // <visibility>0</visibility>
                     foreach(ActivityShort activity in activities)
                     {
+                        // different locations for gpx files.
+                        string outputDir=$"gpx/{activity.AthleteId}";
+                        string outputFilename=$"{activity.ActivityId}_{activity.AthleteId}.gpx";
+                        string outputFilenameGz=$"{activity.ActivityId}_{activity.AthleteId}.gpx.gz";
+                        string outputFilenameErr=$"{activity.ActivityId}_{activity.AthleteId}.err";
+
+                        // skip activities without gpx file at the beginning to avoid node creation in kml file.
+                        if (File.Exists($"{outputDir}/{outputFilenameErr}") ||
+                            (   !File.Exists($"{outputDir}/{outputFilenameGz}") &&
+                                !File.Exists($"{outputDir}/{outputFilename}")
+                            ))
+                        {
+                            // Console.WriteLine($"Skip activity {activity}");
+                            continue;
+                        }
+
+
                         if (lastAthleteId==null)
                         {
                             // first round init athlete
@@ -109,13 +144,14 @@ namespace Strava.XApi.Tools
                                 ,new XElement(kml+"visibility","0")
                                 ,new XElement(kml+"open","0"));
                             StravaXApiFolder.Add(currentAthleteFolder);
-                            lastAthleteId=activity.AthleteId;
                             // first round init activity
                             currentActivityTypeFolder=new XElement(kml+"Folder"
                                 ,new XElement(kml+"name",activity.ActivityType.ToString())
                                 ,new XElement(kml+"open","0"));
                             currentAthleteFolder.Add(currentActivityTypeFolder);
+                            lastAthleteId=activity.AthleteId;
                             lastActivityType=activity.ActivityType;
+                            AthleteCount=1;
                         }
                         else
                         {
@@ -134,6 +170,19 @@ namespace Strava.XApi.Tools
                             }
                             else
                             {
+                                AthleteCount++;
+                                // if splitBlockSize has been set, we may need to write the kml file.
+                                if (splitBlockSize!=int.MaxValue && AthleteCount%splitBlockSize==0)
+                                {
+                                    // end of athlete block size reached, save a kml file.
+                                    saveKmlFile(StravaXApiFolder, exportKmlPath.Replace(filenameExtention,$"_{(AthleteCount/splitBlockSize):D2}{filenameExtention}"), logger);
+                                    // begin a new tree.
+                                    StravaXApiFolder=new XElement(kml+"Folder"
+                                        ,new XElement(kml+"name","Strava-X-Api")
+                                        ,new XElement(kml+"open","1")
+                                    );
+                                }
+
                                 // start a new athlete folder
                                 AthleteShort athlete = db.AthleteShortDB.Find(activity.AthleteId);
                                 string athleteName=athlete.AthleteName;
@@ -148,17 +197,19 @@ namespace Strava.XApi.Tools
                                     ,new XElement(kml+"open","0"));
                                 currentAthleteFolder.Add(currentActivityTypeFolder);
                                 lastAthleteId=activity.AthleteId;
+                                lastActivityType=activity.ActivityType;
                             }
                         }
 
-                        string outputDir=$"gpx/{activity.AthleteId}";
-                        string outputFilename=$"{activity.ActivityId}_{activity.AthleteId}.gpx";
-                        string outputFilenameGz=$"{activity.ActivityId}_{activity.AthleteId}.gpx.gz";
-                        string outputFilenameErr=$"{activity.ActivityId}_{activity.AthleteId}.err";
-
                         try
                         {
-                            if (File.Exists($"{outputDir}/{outputFilenameGz}"))
+                            // read the gpx file and add it to the kml node.
+                            if (File.Exists($"{outputDir}/{outputFilenameErr}"))
+                            {
+                                // Skip error file.
+                                logger.LogWarning($"SKIP: GPX not availlable for {activity.ActivityId} {lastAthleteId} {lastActivityType}");
+                            }
+                            else if (File.Exists($"{outputDir}/{outputFilenameGz}"))
                             {
                                 using(FileStream compressedFileStream = File.OpenRead($"{outputDir}/{outputFilenameGz}"))
                                 {
@@ -175,11 +226,7 @@ namespace Strava.XApi.Tools
                                     currentActivityTypeFolder.Add(handleGpxStream(fileStream, exportType, activity));
                                 } 
                             }
-                            else if (File.Exists($"{outputDir}/{outputFilenameErr}"))
-                            {
-                                // Skip error file.
-                                logger.LogWarning($"SKIP: GPX not availlable for {activity.ActivityId} {lastAthleteId} {lastActivityType}");
-                            }
+                            // else skip, because GPX is not availlable.                            
                         }
                         catch(Exception e)
                         {
@@ -191,16 +238,8 @@ namespace Strava.XApi.Tools
                             Console.WriteLine($"{Count}/{activities.Count()}");
                         }
                     }
-                    XDocument kmlDoc = new XDocument(
-                        new XDeclaration("1.0", "UTF-8", "yes"),
-                        new XElement(kml+"kml"
-                        ,new XAttribute("xmlns", kml)
-                        ,new XAttribute(XNamespace.Xmlns+"gx",gx)
-                        ,new XAttribute(XNamespace.Xmlns+"kml", kml)
-                        ,new XAttribute(XNamespace.Xmlns+"atom", atom)
-                        ,StravaXApiFolder));
-                    kmlDoc.Save(exportKmlPath);
-                    logger.LogInformation($"exported file at {exportKmlPath} ");
+                    string exportKmlPathForSplit = (splitBlockSize==int.MaxValue)?exportKmlPath:exportKmlPath.Replace(filenameExtention,$"_{(AthleteCount/splitBlockSize)+1:D2}{filenameExtention}");
+                    saveKmlFile(StravaXApiFolder, exportKmlPathForSplit, logger);
                 }
                 ret = 0;
             }
@@ -212,7 +251,19 @@ namespace Strava.XApi.Tools
             return ret;
 
         }
-
+        private static void saveKmlFile(XElement StravaXApiFolder, String exportKmlPath, ILogger logger)
+        {
+            XDocument kmlDoc = new XDocument(
+                new XDeclaration("1.0", "UTF-8", "yes"),
+                new XElement(kml+"kml"
+                ,new XAttribute("xmlns", kml)
+                ,new XAttribute(XNamespace.Xmlns+"gx",gx)
+                ,new XAttribute(XNamespace.Xmlns+"kml", kml)
+                ,new XAttribute(XNamespace.Xmlns+"atom", atom)
+                ,StravaXApiFolder));
+            kmlDoc.Save(exportKmlPath);
+            logger.LogInformation($"exported file at {exportKmlPath} ");
+        }
         private static XElement handleGpxStream(Stream stream, ExportType exportType, ActivityShort activity)
         {
             XElement GpxToKmlElt;
@@ -231,9 +282,13 @@ namespace Strava.XApi.Tools
             }
             return GpxToKmlElt;
         }
-        /**
-         * Extract all points from the gpx file and fill an XElement which could be integrate in the kml file.
-         */
+        /// <summary>
+        /// Extract all points from the gpx file and fill an XElement which could be integrate in the kml file. 
+        /// </summary>
+        /// <param name="activity"></param>
+        /// <param name="stream"></param>
+        /// <param name="kml"></param>
+        /// <returns></returns>
         public static XElement readGpx(ActivityShort activity, Stream stream,XNamespace kml)
         {
             XDocument gpxDoc = XDocument.Load(stream);
@@ -270,6 +325,17 @@ namespace Strava.XApi.Tools
             return kmlGpxDocument;
 
         }
+        
+        /// <summary>
+        /// Convert a gpx file in kml.
+        /// Estimate the timestamp of each point, depending on the activity start and its duration.
+        /// This make the timeline navigation in Google-Earth possible.
+        /// </summary>
+        /// <param name="activity"></param>
+        /// <param name="stream"></param>
+        /// <param name="kml"></param>
+        /// <param name="gx"></param>
+        /// <returns></returns>//     
         public static XElement convertToKmlWithTime(ActivityShort activity, Stream stream,XNamespace kml,XNamespace gx)
         {
             XDocument gpxDoc = XDocument.Load(stream);
@@ -345,7 +411,6 @@ namespace Strava.XApi.Tools
                         coordList
             )));
             return kmlGpxDocument;
-
         }
     }
 }
